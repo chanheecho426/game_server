e3697033660a6360a4e4d018b8b327ab878ba615
fs426 <81139548+fs426@users.noreply.github.com>
2023-07-07 21:54:18 +0900

    Adding TODO and ending

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 //FRONTEND
-
+//TODO - Know the fuck what this does
 const socket = io('ws://localhost:5000');
-
+//TODO - Know the fuck what this does
 socket.on("connect",()=>{console.log("logged")});
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 //BACKEND
 
+//TODO - Know the fuck what this does
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-
+//TODO - Know the fuck what this does
 const app = express();
 const httpServer = createServer(app);
-
+//TODO - Know the fuck what this does
 const io = new Server(httpServer);
-
+//TODO - Know the fuck what this does
 io.on("connect", (socket) => {
     console.log("user connected",socket.id);
 });
-
+//TODO - Know the fuck what this does
 app.use(express.static("public"));
 
 httpServer.listen(5000,()=>{console.log("server running")});
